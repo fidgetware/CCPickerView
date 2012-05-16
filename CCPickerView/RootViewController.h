f7d6cba63b311e0d587629c72824514207ac0e12
Mick Lester <mick@Micks-MacBook-Pro.local>
2012-05-16 08:56:59 -0600

    Initial Commit

---
+++ CCPickerView/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  CCPickerView
+//
+//  Created by Mick Lester on 5/16/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ CCPickerView/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CCPickerView
+//
+//  Created by Mick Lester on 5/16/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
